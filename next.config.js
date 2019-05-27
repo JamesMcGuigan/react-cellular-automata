b41506d0589f5dc712f1b214e8ddd064b20ef90f
JamesMcGuigan <james.mcguigan.github@gmail.com>
2019-05-27 17:02:30 +0100

    next.js | partially remove fsevents as package.json dependency

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 // DOCS: https://github.com/zeit/next-plugins/tree/master/packages/next-typescript
 
 const withTypescript = require("@zeit/next-typescript");
-const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
 module.exports = withTypescript({
   webpack(config, options) {
