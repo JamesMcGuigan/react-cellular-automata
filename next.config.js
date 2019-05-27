7615983529d16eb8b2ec9f36da3ff5fb50d0e383
JamesMcGuigan <james.mcguigan.github@gmail.com>
2019-05-27 17:00:36 +0100

    next.js | partially remove fsevents as package.json dependency

--- next.config.js
+++ next.config.js
@@ -5,14 +5,6 @@ const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
 module.exports = withTypescript({
   webpack(config, options) {
-    // If your IDE or code editor don't provide satisfying TypeScript support,
-    // or you want to see error list in console output, you can use fork-ts-checker-webpack-plugin.
-    // It will not increase compile time because it forks type checking in a separate process
-    if (options.isServer) {
-      // Do not run type checking twice:
-      config.plugins.push(new ForkTsCheckerWebpackPlugin());
-    }
-
     return config;
   }
 });
