6bcbd705f8c1814faa916e9bd52ac2228da71b63
JamesMcGuigan <james.mcguigan.github@gmail.com>
2019-05-27 14:08:21 +0100

    next.js | implement tslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 // DOCS: https://levelup.gitconnected.com/ultimate-react-component-patterns-with-typescript-2-8-82990c516935
 // DOCS: https://www.typescriptlang.org/docs/handbook/functions.html
 
-import React, { FunctionComponent } from 'react'
+import * as React from "react";
 // function Home():FunctionComponent<{}> {
 //     return <div>Welcome to Next.js!</div>
 // };
 
-const Home: FunctionComponent = () => {
-    return <div>Welcome to Next.js!!!</div>
+const Home: React.FunctionComponent = () => {
+    return (
+        <div>Welcome to Next.js!!!</div>
+    );
 };
 
-export default Home
+export default Home;
