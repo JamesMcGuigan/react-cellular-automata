50be742153f3b6ead37f36ebf6686e664d0d372e
JamesMcGuigan <james.mcguigan.github@gmail.com>
2019-06-03 18:37:31 +0100

    index.tsx | Add ForkMeOnGithub banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,18 @@
 // DOCS: https://www.typescriptlang.org/docs/handbook/functions.html
 
 import * as React          from 'react';
+import ForkMeOnGithub      from "../src/components/ForkMeOnGithub";
 import GameOfLifeComponent from "../src/components/GameOfLife/GameOfLifeComponent";
 
 
 const Home: React.FunctionComponent<JSX.Element> = () => {
     return (
-        <div style={{ maxWidth: "1000px", margin: "auto" }}>
-            <GameOfLifeComponent/>
-        </div>
+        <>
+            <ForkMeOnGithub/>
+            <div style={{ maxWidth: "1000px", margin: "auto" }}>
+                <GameOfLifeComponent/>
+            </div>
+        </>
     );
 };
 
